090bf8ee186767c29a3629c097c3969554006509
jianingwang124 <jianingwang124@gmail.com>
2024-03-23 19:21:04 +0800

    added config.py back

---
+++ server/config.py
@@ -0,0 +1,14 @@
+import os 
+
+baseurl = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    H2O_API_KEY = 'sk-33L5yNLSb3gX4GwPjZf7sV5o9pI3YgeY1E0kj5yHI7ajqbAs'
+    H2O_COLLECTIOM_API_KEY = 'sk-QxCDdaRYFHrw3fZLQ8b0EcQmikRNENWqsPkrXi2af928q1KB'
+    H2O_ADDRESS = 'https://h2ogpte.genai.h2o.ai'
+    UPLOAD_FOLDER = './docs'
+
+    MYSQL_HOST = 'localhost:3306'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'passwd'
+    MYSQL_DB = 'dsa4213'
