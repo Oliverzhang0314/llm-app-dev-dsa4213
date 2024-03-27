1f223c0fd62be35dcc98c9cdd6bf9fbd62398556
Zhan Yi <liauzhanyi@gmail.com>
2024-03-27 21:54:30 +0800

    Update recommendation function

--- server/flask_app/services/recommendation_service.py
+++ server/flask_app/services/recommendation_service.py
@@ -1,20 +1,38 @@
-from h2ogpte import H2OGPTE
-# from flask import current_app as app, jsonify, request
 import os
 from dotenv import load_dotenv
+import mysql.connector
 
 load_dotenv(os.path.join("..", "..", ".env"))
 
-client = H2OGPTE(
-    address=os.getenv('H2O_ADDRESS'),
-    api_key=os.getenv('H2O_COLLECTIOM_API_KEY'),
-)
+# Database configuration
+db_config = {
+    'host': os.getenv('MYSQL_HOST'),
+    'user': os.getenv('MYSQL_USER'),
+    'password': os.getenv('MYSQL_PASSWORD'),
+    'database': os.getenv('MYSQL_DB'),
+    'port': os.getenv('MYSQL_PORT')
+}
 
-#use an exsited collection and create new chat session
-chat_session_id = client.create_chat_session_on_default_collection()
-default_collection = client.get_default_collection()
-collection_id = default_collection.id
-# collection_id = client.create_collection(
-#     name='Resume',
-#     description='ResumeAnalysis',
-# )
+# Route to handle the query
+def recommendation(k=10):
+    try:
+        # Connect to the database
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        # Execute the query
+        query = f"SELECT * FROM candidates LIMIT {k};"
+        cursor.execute(query)
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+        print('Query executed successfully')
+        return rows
+
+    except mysql.connector.Error as error:
+        return f'Error executing query: {error}'
