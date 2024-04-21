ab8bfe1f0f11d3b1203445da05dba648076dd133
Zhan Yi <liauzhanyi@gmail.com>
2024-04-21 20:31:54 +0800

    Restore lost endpoints

--- server/flask_app/routes.py
+++ server/flask_app/routes.py
@@ -6,21 +6,12 @@ from .services.rag_service import *
 from .services.recommendation_service import *
 from .services.profile_service import *
 
+
 @app.route('/')
 def index():
-    """
-    Endpoint for the root URL.
-    """
-    return "Welcome to the DSA4213 Group Whisper server"
-
+    return "welcome to flask server"
 @app.route('/file/upload', methods=['GET','POST'])
 def upload_file():
-    """
-    Endpoint for uploading files and generating candidate profile.
-
-    Returns:
-        Response: JSON response indicating success or failure.
-    """
     if 'file' not in request.files:
         error_message = jsonify({'status':'No file uploaded.'})
         response = make_response(error_message, 400)
@@ -44,20 +35,14 @@ def upload_file():
                 return jsonify({'error': str(e)}), 500
 
     try:
-        # parese reqeust to get arguments
-        
-        # position = request.form.get('position')
-        # region = request.form.get('region')
-        # department = request.form.get('department')
-        
-        position = "software engineer"
-        region = "singapore"
-        department = "swe"
-        
-        # generate candidate profile
-        profile = create_profile(filenames, position, region, department)
+        # rag_file_upload_service(filenames)
+        position = request.form.get('position')
+        region = request.form.get('region')
+        department = request.form.get('department')
+
+        profile = create_profile(position, region, department)
         data={
-            'status': 'File uploaded successfully',
+            'status': 'File uploaded suceessfully',
             'data' : profile
         }
         response = make_response(data, 200)
@@ -65,14 +50,9 @@ def upload_file():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/rag/query', methods=['POST'])
+#TODO: create rag_query api by using rag_query_service
+@app.route('/rag/query', methods=['GET','POST'])
 def rag_query():
-    """
-    Endpoint for querying the RAG service.
-
-    Returns:
-        Response: JSON response with replies from the RAG service.
-    """
     try:
         queries = request.get_json().get('queries')
         replies = rag_query_service(queries)
@@ -82,3 +62,45 @@ def rag_query():
     except Exception as e:
         return jsonify({'error':str(e)}), 500
         
+@app.route('/rag/summary', methods=['GET','POST'])
+def rag_summary():
+    try:
+        filenames=request.get_json().get('filenames')
+        summaries = rag_summary_service(filenames)
+        success_message = jsonify({"summaries":summaries})
+        response = make_response(success_message,200)
+        return response
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+ 
+@app.route('/candidate/recommendation/table', methods=['GET'])
+def candidate_rank():
+    try:
+        position = request.args.get('position', "position_applied")
+        region = request.args.get('region', "region")
+        dept = request.args.get('dept', "department")
+        limit = request.args.get('limit', 10)
+        return jsonify(candidates_table(position, region, dept, limit))
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/candidate/recommendation/radar-plot', methods=['GET'])
+def candidate_radar_plot():
+    try:
+        position = request.args.get('position', "position_applied")
+        region = request.args.get('region', "region")
+        dept = request.args.get('dept', "department")
+        limit = request.args.get('limit', 4)
+        return jsonify(radar_plot(position, region, dept, limit))
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/candidate/experience-levels', methods=['GET'])
+def candidate_experience_distribution():
+    try:
+        position = request.args.get('position', "position_applied")
+        region = request.args.get('region', "region")
+        dept = request.args.get('dept', "department")
+        return jsonify(experience_distribution(position, region, dept))
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
